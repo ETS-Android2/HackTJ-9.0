16d081258f0976917aed15b64c1d2910c85cc9f0
dhshin04 <dhshin0301@gmail.com>
2022-04-16 18:00:50 -0400

    Added a Splash Screen

---
+++ SplashScreen/app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
