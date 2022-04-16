9000a2dca94ba9a391a3d48d56c2b7dab49522ee
Ethan Ye <ethan.ye0312@gmail.com>
2022-04-16 19:10:52 -0400

    Prototype 1

--- TensorFlowLitePractice/app/src/main/java/com/example/tensorflowlitepractice/MainActivity.java
+++ TensorFlowLitePractice/app/src/main/java/com/example/tensorflowlitepractice/MainActivity.java
@@ -15,19 +15,13 @@ import org.tensorflow.lite.task.vision.detector.Detection;
 import org.tensorflow.lite.task.vision.detector.ObjectDetector;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.os.Bundle;
 import android.provider.MediaStore;
-import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 import java.util.List;
 
@@ -57,16 +51,6 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(MainActivity.this,
                     new String[]{Manifest.permission.CAMERA}, 101);
         }
-//        cameraButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//                startActivityForResult(intent, 101);
-//            }
-//        });
-
-
-
     }
 
     public Bitmap convertToBitmap(){
